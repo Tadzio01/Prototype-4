9cc07ca9461626b8745b8ebcf83121b99303b7bb
Tadzio <tadziocancilla@gmail.com>
2020-02-21 11:53:40 -0800

    Add controls

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject fp;
+    private Rigidbody rb;
+    private float speed = 150;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        fp = GameObject.Find("FocalPoint");
+    }
+
+    void Update()
+    {
+        float fi = Input.GetAxis("Vertical");
+        rb.AddForce(fp.transform.forward*fi*speed*Time.deltaTime);
+    }
+}
