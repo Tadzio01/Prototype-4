9d184f9ae15540d48a22342fe10cc51a84771e8c
Tadzio <tadziocancilla@gmail.com>
2020-02-25 11:28:10 -0800

    Basic enemy spawning and functionality

---
+++ Assets/Course Library/Scripts/EnemyFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    private float speed = 3.0f;
+    private Rigidbody rb;
+    private GameObject p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        p = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 ld = (p.transform.position-transform.position).normalized;
+        rb.AddForce(ld*speed);
+    }
+}
---
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject prefab;
+    private float range = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab,gen(),Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private Vector3 gen(){
+        float x = Random.Range(-range,range);
+        float z = Random.Range(-range,range);
+        return new Vector3(x,0,z);
+    }
+
+}
